a849596bc82ce860f30ddad6a9bbefa971fe2754
DeepSwarup <deepswarup1@gmail.com>
2025-07-08 20:16:39 +0530

    fix callback

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -17,7 +17,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: 'http://localhost:5000/auth/google/callback',
+      // callbackURL: 'http://localhost:5000/auth/google/callback',
+      callbackURL: 'https://note-app-somf.onrender.com/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
